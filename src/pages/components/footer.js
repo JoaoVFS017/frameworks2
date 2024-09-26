62b7b7535f4671e12407e71e4030f729e4e18903
JoaoVFS017 <119964116+JoaoVFS017@users.noreply.github.com>
2024-09-26 21:41:16 +0000

    aula dia 26/09

---
+++ src/pages/components/footer.js
@@ -0,0 +1,4 @@
+function footer(){
+    return<h1>Footer</h1>
+}
+export default footer
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Navbar from "./components/navbar"
+
+export default function Index(){
+  return <h1>Olá Next</h1>
+}
